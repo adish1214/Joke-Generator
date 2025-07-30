bb9a90c345d047980cf817862835dea491fe47fe
Adish <adishvaishnavam@gmail.com>
2025-07-30 14:36:27 +0530

    Initial Commit

---
+++ script.js
@@ -0,0 +1,16 @@
+const jokeBtn=document.querySelector(".joke-generator")
+jokeBtn.addEventListener('click',()=>{
+    getJoke();
+})
+
+async function getJoke() {
+      const url = "https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw&type=single";
+      try {
+        const res = await fetch(url);
+        const data = await res.json();
+        document.getElementById("joke-section").textContent = data.joke || "No joke found!";
+      } catch (error) {
+        document.getElementById("joke-section").textContent = "Failed to fetch a joke 😞";
+        console.error(error);
+      }
+    }
